dd49b90d66f93de30bd4961af7c865db13ac5f1f
priowisiso <wcici504@gmail.com>
2022-04-21 00:35:01 +0700

    first commit

---
+++ WP2_17190146/application/controllers/contoh_1.php
@@ -0,0 +1,13 @@
+<?php 
+class contoh_1 extends CI_Controller 
+{ 
+public function index() 
+{ 
+    echo "<h1>Biodata</h1>"; 
+    echo "Nama : Prio Wisiso<br>";
+    echo "Nim : 17190146<br>";
+    echo "Jurusan :Teknologi Informasi<br>";
+    echo "Fakultas : Teknik dan Informatika<br>";
+    echo "Hobby : Main Bola";
+} 
+}
